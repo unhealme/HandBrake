58f386d129f5a66c461c07ba94edf14d5d5e0893
unhealme <o2tongpethuk@gmail.com>
2025-08-08 02:32:15 +0700

    fix build error due to cargo permissions

--- scripts/builder/build_win64.sh
+++ scripts/builder/build_win64.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
 cd /HandBrake/src || exit 1
-scripts/mingw-w64-build x86_64 "${HOME}/handbrake/toolchains/"
+scripts/mingw-w64-build -j "$(nproc)" x86_64 "${HOME}/handbrake/toolchains/"
 PATH="${HOME}/handbrake/toolchains/mingw-w64-x86_64/bin:${PATH}" ./configure --enable-fdk-aac --enable-libdovi --enable-nvdec --cross=x86_64-w64-mingw32 --launch-jobs="$(nproc)" --launch
