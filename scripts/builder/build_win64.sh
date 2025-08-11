d974f52b1ec489e3a5ba77cf57f96372fcac3890
unhealme <o2tongpethuk@gmail.com>
2025-08-12 01:05:30 +0700

    overwrite build by default

--- scripts/builder/build_win64.sh
+++ scripts/builder/build_win64.sh
@@ -2,4 +2,4 @@
 
 cd /HandBrake/src || exit 1
 scripts/mingw-w64-build -j "$(nproc)" x86_64 "${HOME}/handbrake/toolchains/"
-PATH="${HOME}/handbrake/toolchains/mingw-w64-x86_64/bin:${PATH}" ./configure --enable-fdk-aac --enable-libdovi --enable-nvdec --cross=x86_64-w64-mingw32 --launch-jobs="$(nproc)" --launch
+PATH="${HOME}/handbrake/toolchains/mingw-w64-x86_64/bin:${PATH}" ./configure --enable-fdk-aac --enable-libdovi --enable-nvdec --cross=x86_64-w64-mingw32 --launch-jobs="$(nproc)" --force --launch
