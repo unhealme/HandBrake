19a369d26d5feff549ccd848d30ef800f54d5ef2
unhealme <o2tongpethuk@gmail.com>
2025-08-12 00:46:09 +0700

    check for dockerfile update

--- scripts/builder/run.sh
+++ scripts/builder/run.sh
@@ -2,5 +2,9 @@
 BASEDIR="$(dirname -- "$(readlink -f -- "$0")")" || exit $?
 IMAGE='ghcr.io/unhealme/handbrake-builds/builder:latest'
 
-[[ -n "$(docker images -q "$IMAGE")" ]] || docker build -t ghcr.io/unhealme/handbrake-builds/builder:latest "$BASEDIR"
+CREATED="$(docker inspect --format '{{.Created}}' "$IMAGE")"
+if [[ "$(stat -c '%Y' -- "$BASEDIR"/Dockerfile)" -gt "$(date -d "$CREATED" '+%s')" ]]; then
+    [[ -n "$CREATED" ]] && docker rmi "$IMAGE"
+    docker build -t "$IMAGE" "$BASEDIR" || exit 1
+fi
 docker run --rm -u 1000:1000 -v "$BASEDIR"/../..:/HandBrake/src:shared --mount type=bind,src="$BASEDIR"/build_win64.sh,dst=/build.sh,ro "$IMAGE" bash /build.sh
