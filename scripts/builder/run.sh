0bb20c08d6450a1af84b28ebc81298de512cbb9c
unhealme <o2tongpethuk@gmail.com>
2025-08-12 01:28:55 +0700

    fix fetching dockerfile mt

--- scripts/builder/run.sh
+++ scripts/builder/run.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/bash
 BASEDIR="$(dirname -- "$(readlink -f -- "$0")")" || exit $?
+GITDIR="$(realpath -se -- "$BASEDIR"/../..)" || exit $?
 IMAGE='ghcr.io/unhealme/handbrake-builds/builder:latest'
 
 CREATED="$(docker inspect --format '{{.Created}}' "$IMAGE")"
-if [[ "$(stat -c '%Y' -- "$BASEDIR"/Dockerfile)" -gt "$(date -d "$CREATED" '+%s')" ]]; then
+if [[ "$(git -C "$GITDIR" log -1 --pretty="format:%ct" -- "$BASEDIR"/Dockerfile)" -gt "$(date -d "$CREATED" '+%s')" ]]; then
     [[ -n "$CREATED" ]] && docker rmi "$IMAGE"
     docker build -t "$IMAGE" "$BASEDIR" || exit 1
 fi
-docker run --rm -u 1000:1000 -v "$BASEDIR"/../..:/HandBrake/src:shared --mount type=bind,src="$BASEDIR"/build_win64.sh,dst=/build.sh,ro "$IMAGE" bash /build.sh
+docker run --rm -u 1000:1000 -v "$GITDIR":/HandBrake:shared --mount type=bind,src="$BASEDIR"/build_win64.sh,dst=/build.sh,ro "$IMAGE" bash /build.sh
