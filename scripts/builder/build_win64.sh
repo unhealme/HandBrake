2bcefb2b100708c5b736237e47197fd3d1af685f
unhealme <o2tongpethuk@gmail.com>
2025-08-26 02:16:51 +0700

    revert: check for dockerfile update

--- scripts/builder/build_win64.sh
+++ scripts/builder/build_win64.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-cd /HandBrake/src || exit 1
+cd /HandBrake || exit 1
 scripts/mingw-w64-build -j "$(nproc)" x86_64 "${HOME}/handbrake/toolchains/"
 PATH="${HOME}/handbrake/toolchains/mingw-w64-x86_64/bin:${PATH}" ./configure --enable-fdk-aac --enable-libdovi --enable-nvdec --cross=x86_64-w64-mingw32 --launch-jobs="$(nproc)" --force --launch
--- scripts/builder/run.sh
+++ scripts/builder/run.sh
@@ -3,9 +3,5 @@ BASEDIR="$(dirname -- "$(readlink -f -- "$0")")" || exit $?
 GITDIR="$(realpath -se -- "$BASEDIR"/../..)" || exit $?
 IMAGE='ghcr.io/unhealme/handbrake-builds/builder:latest'
 
-CREATED="$(docker inspect --format '{{.Created}}' "$IMAGE")"
-if [[ "$(git -C "$GITDIR" log -1 --pretty="format:%ct" -- "$BASEDIR"/Dockerfile)" -gt "$(date -d "$CREATED" '+%s')" ]]; then
-    [[ -n "$CREATED" ]] && docker rmi "$IMAGE"
-    docker build -t "$IMAGE" "$BASEDIR" || exit 1
-fi
+[[ -n "$(docker images -q "$IMAGE")" ]] || docker build -t "$IMAGE" "$BASEDIR" || exit 1
 docker run --rm -u 1000:1000 -v "$GITDIR":/HandBrake:shared --mount type=bind,src="$BASEDIR"/build_win64.sh,dst=/build.sh,ro "$IMAGE" bash /build.sh
